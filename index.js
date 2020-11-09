679eb16d9f72db999e81b69d6a48a00a33d96979
Jenny <jennyhan_wang@outlook.com>
2020-11-09 20:13:59 +0800

    navbar

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const http = require('http');
 const Express = require('express');
 const path = require('path');
 
-console.log(path.resolve('..'));
+console.log(path.resolve('.'));
 app = Express();
-app.use(Express.static('..'));
+app.use(Express.static('.'));
 
 http.createServer(app).listen(8080);
 console.log('Server is running on port 8080');
