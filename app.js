f791d4e7451ff52aa96bccf3e87450f6df250e79
Jenny <jennyhan_wang@outlook.com>
2020-11-12 14:12:51 +0800

    installed jquery

--- app.js
+++ app.js
@@ -28,7 +28,7 @@
 let deferredPrompt = null;
 
 window.addEventListener("beforeinstallprompt", (e) => {
-  $("#install-button").show();
+  // $("#install-button").show();
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
